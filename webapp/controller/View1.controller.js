1b2e2bffd7a58f7a951bf6c63cbeda172d32ea42
nune chandini chandini <chandininune19@gmail.com>
2023-11-04 17:37:21 +0000

    first commit

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,65 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/Fragment"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller,Fragment) {
+        "use strict";
+
+        return Controller.extend("sap1.controller.View1", {
+            onInit: function () {
+             let oData = {
+              bankdetails:{
+                accountnum : "12345678",
+                holdername: "chandini",
+                ifsc: "idb*******",
+                id:"345677",
+                address:{
+                    city:"hyderabad",
+                    postalcode:"534222",
+                    country:"india"
+                }
+              },
+              ifsccode:"00000000010000"
+            };
+            let oModel= new sap.ui.model.json.JSONModel();
+            oModel.setData(oData);
+            this.getView().setModel(oModel);
+            },
+
+            // findmoredetails: function(){
+            //  var oview =this.getView();
+            //     if(!this.byId("dialog1")){
+            //       Fragment.load({
+            //         name:"sap1.view.moredetail",
+            //         controller:this,
+            //         id:oview.getId()
+            //       }).then(function(oDialog){
+            //          oDialog.open();
+            //       });
+            //  }else{
+            //     this.byId("dialog1").open();
+            //  }
+            // },
+
+            onclose: function(){
+              this.byId("dialog1").close();
+            },
+            
+            
+            findmoredetails: function(){
+              if(!this.dialog1){
+                this.dialog1 = this.loadFragment(
+                  { name : "sap1.view.moredetail"}
+                  );
+              }this.dialog1.then(
+                function(oDialog){
+                  oDialog.open();
+                }
+              );
+            }
+
+        });
+    });
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"sap1/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
